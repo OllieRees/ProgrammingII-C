e6f7cb020ab3284bb63b7074c325773b701451d1
Ollie Rees (or1g18) <or1g18@soton.ac.uk>
2019-05-17 12:52:00 +0100

    Added null pointer checking and checking if N is valid in checkNEW(). Also included error messages.

--- question_2/q2.c
+++ question_2/q2.c
@@ -9,27 +9,46 @@ int checkSTORE(CHECKARR as, int N, int row, int col, int val);
 int checkFETCH(CHECKARR as, int N, int row, int col);
 
 CHECKARR checkNEW(int N) {
-	int * arr = malloc(((1 + (N * N)) * sizeof(int))/2);
-	memset(arr, 0, ((1 + (N * N)) * sizeof(int))/2);
-	return arr;
+	
+	if(N <= 0) {
+		printf("No board.\n");
+		exit(0);
+	}
+
+	if(N > 0) {
+		int* arr = malloc(((1 + (N * N)) * sizeof(int))/2);
+		memset(arr, 0, ((1 + (N * N)) * sizeof(int))/2);
+		return arr;
+	}
 }	
 
 int checkSTORE(CHECKARR as, int N, int row, int col, int val) {
+	
+	//check for null pointer
+	if(as == NULL) {
+		printf("Uh oh! Looks like you're using a null pointer.\n");
+		return -1;
+	}
+
 	//invalid N
 	if(N <= 0) {
+		printf("Your board doesn't exist.\n");
 		return -1;	
 	}
 
 	//index out of bounds
 	if(row >= N || col >= N) {
+		printf("Java.lang.Exception.IndexOutOfBoundsException e {e.printStackTrace()}\n");
 		return -1;
 	}
 	if(row < 0 || col < 0) { 
+		printf("Rows and columns are less than 0.\n");
 		return -1;
 	}
 
 	//improper rules
 	if(row % 2 != col % 2) {
+		printf("You're playing checkers. You can only move to squares with the same parity.\n");
 		return -1;
 	}
 	
@@ -40,21 +59,31 @@ int checkSTORE(CHECKARR as, int N, int row, int col, int val) {
 
 int checkFETCH(CHECKARR as, int N, int row, int col) {
 	
+	//check for null pointer
+	if(as == NULL) {
+		printf("Uh oh! Looks like you're using a null pointer.\n");
+		return -1;
+	}
+
 	//invalid N
 	if(N <= 0) {
+		printf("Your board doesn't exist.\n");
 		return -1;
 	}
 
 	//index out of bounds
 	if(row < 0 || col < 0) { 
+		printf("Java.lang.Exception.IndexOutOfBoundsException e {e.printStackTrace()}\n");
 		return -1;
 	}
 	if(row >= N || col >= N) { 
+		printf("Rows and columns are less than 0.\n");
 		return -1;
 	}	
 	
 	//improper rules
 	if(row % 2 != col % 2) {
+		printf("You're playing checkers. You can only move to squares with the same parity.\n");
 		return -1;
 	}
 	
